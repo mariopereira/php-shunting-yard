797f0748f4bf6b1a409207f77c51a6958d60514d
andig <cpuidle@gmx.de>
2014-01-16 12:22:02 +0100

    Added support for string constants

--- example.php
+++ example.php
@@ -10,9 +10,7 @@ use RR\Shunt\Context;
 
 // einfach
 $trm = '3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3';
-print Parser::parse($trm); // 3.0001220703125
-
-print "\n";
+print Parser::parse($trm)."\n"; // 3.0001220703125
 
 // mit eigenen konstanten und funktionen
 $ctx = new Context;
@@ -21,4 +19,12 @@ $ctx->def('foo', 5);
 $ctx->def('bar', function($a, $b) { return $a * $b; });
 
 $trm = '3 + bar(4, 2) / (abs(-1) - foo) ^ 2 ^ 3';
-print Parser::parse($trm, $ctx); // 3.0001220703125
+print Parser::parse($trm, $ctx)."\n"; // 3.0001220703125
+
+// mit string konstanten
+$ctx = new Context;
+$ctx->def('groupA', 'A', 'string'); // string constant
+$ctx->def('isgroupA', function($g) { return ($g == 'A') ? 1 : 0; });
+
+$trm = 'isgroupA(groupA)';
+print Parser::parse($trm, $ctx)."\n"; // 1
