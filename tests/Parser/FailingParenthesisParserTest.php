723126f65d9d70079f3657748e00235eedc4e2ef
Soroush <s_falahati@hotmail.com>
2014-11-26 18:29:35 +0330

    test type changed to output test

--- tests/Parser/FailingParenthesisParserTest.php
+++ tests/Parser/FailingParenthesisParserTest.php
@@ -7,9 +7,7 @@ use RR\Shunt\Exception\RuntimeError;
 
 class FailingParenthesisParserTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException \RR\Shunt\Exception\RuntimeError
-     */
+	
     public function testParenthesisThrowsError()
     {
 		$this->expectOutputString(null);
